30be71ad79d20e6e4dc479fd2175f1610c1d6848
Wren Powell <wrenp@duck.com>
2024-03-30 13:12:27 -0400

    Add additional tests for files

--- tests/file.rs
+++ tests/file.rs
@@ -1,10 +1,13 @@
+use std::io::prelude::*;
 use std::path::Path;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
+use rand::rngs::SmallRng;
+use rand::{prelude::*, SeedableRng};
 use sqlarfs::{Compression, Connection, ErrorKind, FileMetadata, FileMode};
 use xpct::{
-    all, approx_eq_time, be_err, be_false, be_none, be_ok, be_some, be_true, be_zero, equal,
-    expect, fields, match_fields, match_pattern, pattern,
+    all, approx_eq_time, be_err, be_false, be_none, be_ok, be_some, be_true, be_zero, eq_diff,
+    equal, expect, fields, match_fields, match_pattern, pattern,
 };
 
 fn connection() -> sqlarfs::Result<Connection> {
@@ -13,6 +16,13 @@ fn connection() -> sqlarfs::Result<Connection> {
     conn.exec(|archive| archive.init())?;
 
     Ok(conn)
+}
+
+fn random_bytes(len: usize) -> Vec<u8> {
+    let mut buf = Vec::with_capacity(len);
+    let mut rng = SmallRng::from_entropy();
+    rng.fill_bytes(&mut buf);
+    buf
 }
 
 #[test]
@@ -92,7 +102,7 @@ fn file_correctly_reports_that_it_exists() -> sqlarfs::Result<()> {
 #[test]
 fn file_correctly_reports_that_it_does_not_exist() -> sqlarfs::Result<()> {
     connection()?.exec(|archive| {
-        let file = archive.open(Path::new("existing-file"));
+        let file = archive.open(Path::new("nonexistent-file"));
 
         expect!(file.exists()).to(be_ok()).to(be_false());
 
@@ -103,7 +113,7 @@ fn file_correctly_reports_that_it_does_not_exist() -> sqlarfs::Result<()> {
 #[test]
 fn deleting_file_when_it_does_not_exist() -> sqlarfs::Result<()> {
     connection()?.exec(|archive| {
-        let mut file = archive.open(Path::new("existing-file"));
+        let mut file = archive.open(Path::new("nonexistent-file"));
 
         expect!(file.delete())
             .to(be_err())
@@ -131,7 +141,7 @@ fn deleting_file_when_it_does_exist() -> sqlarfs::Result<()> {
 #[cfg(feature = "deflate")]
 fn set_compression_method() -> sqlarfs::Result<()> {
     connection()?.exec(|archive| {
-        let mut file = archive.open(Path::new("existing-file"));
+        let mut file = archive.open(Path::new("file"));
 
         // We specify that the default is DEFLATE compression when the feature flag is enabled, but
         // not what the default compression level is.
@@ -148,7 +158,7 @@ fn set_compression_method() -> sqlarfs::Result<()> {
 #[test]
 fn set_file_mode() -> sqlarfs::Result<()> {
     connection()?.exec(|archive| {
-        let mut file = archive.open(Path::new("existing-file"));
+        let mut file = archive.open(Path::new("file"));
 
         file.create(None)?;
 
@@ -172,9 +182,23 @@ fn set_file_mode() -> sqlarfs::Result<()> {
 }
 
 #[test]
+fn set_file_mode_when_file_does_not_exist() -> sqlarfs::Result<()> {
+    connection()?.exec(|archive| {
+        let mut file = archive.open(Path::new("file"));
+
+        expect!(file.set_mode(None))
+            .to(be_err())
+            .map(|err| err.into_kind())
+            .to(equal(ErrorKind::NotFound));
+
+        Ok(())
+    })
+}
+
+#[test]
 fn set_file_mtime() -> sqlarfs::Result<()> {
     connection()?.exec(|archive| {
-        let mut file = archive.open(Path::new("existing-file"));
+        let mut file = archive.open(Path::new("file"));
 
         file.create(None)?;
 
@@ -199,9 +223,23 @@ fn set_file_mtime() -> sqlarfs::Result<()> {
 }
 
 #[test]
+fn set_file_mtime_when_file_does_not_exist() -> sqlarfs::Result<()> {
+    connection()?.exec(|archive| {
+        let mut file = archive.open(Path::new("file"));
+
+        expect!(file.set_mtime(None))
+            .to(be_err())
+            .map(|err| err.into_kind())
+            .to(equal(ErrorKind::NotFound));
+
+        Ok(())
+    })
+}
+
+#[test]
 fn file_size_is_zero_when_file_is_empty() -> sqlarfs::Result<()> {
     connection()?.exec(|archive| {
-        let mut file = archive.open(Path::new("existing-file"));
+        let mut file = archive.open(Path::new("file"));
 
         file.create(None)?;
 
@@ -216,7 +254,7 @@ fn file_size_is_zero_when_file_is_empty() -> sqlarfs::Result<()> {
 #[test]
 fn file_correctly_reports_being_empty() -> sqlarfs::Result<()> {
     connection()?.exec(|archive| {
-        let mut file = archive.open(Path::new("existing-file"));
+        let mut file = archive.open(Path::new("file"));
 
         file.create(None)?;
 
@@ -229,12 +267,172 @@ fn file_correctly_reports_being_empty() -> sqlarfs::Result<()> {
 #[test]
 fn file_correctly_reports_being_not_empty() -> sqlarfs::Result<()> {
     connection()?.exec(|archive| {
-        let mut file = archive.open(Path::new("existing-file"));
+        let mut file = archive.open(Path::new("file"));
 
         file.create(None)?;
         file.write_str("file contents")?;
 
         expect!(file.is_empty()).to(be_ok()).to(be_false());
+
+        Ok(())
+    })
+}
+
+#[test]
+fn is_file_empty_when_it_does_not_exist() -> sqlarfs::Result<()> {
+    connection()?.exec(|archive| {
+        let file = archive.open(Path::new("file"));
+
+        expect!(file.is_empty())
+            .to(be_err())
+            .map(|err| err.into_kind())
+            .to(equal(ErrorKind::NotFound));
+
+        Ok(())
+    })
+}
+
+#[test]
+fn write_bytes_without_compression() -> sqlarfs::Result<()> {
+    connection()?.exec(|archive| {
+        let mut file = archive.open(Path::new("file"));
+        file.create(None)?;
+
+        file.set_compression(Compression::None);
+
+        let expected = random_bytes(32);
+
+        expect!(file.write_bytes(&expected)).to(be_ok());
+
+        let mut reader = file.reader()?;
+        let mut actual = Vec::with_capacity(expected.len());
+
+        reader.read_to_end(&mut actual)?;
+
+        expect!(&actual).to(eq_diff(&expected));
+
+        drop(reader);
+
+        expect!(file.metadata())
+            .to(be_ok())
+            .map(|metadata| metadata.size)
+            .try_into::<usize>()
+            .to(equal(expected.len()));
+
+        Ok(())
+    })
+}
+
+#[test]
+#[cfg(feature = "deflate")]
+fn write_incompressible_bytes_with_compression() -> sqlarfs::Result<()> {
+    connection()?.exec(|archive| {
+        let mut file = archive.open(Path::new("file"));
+        file.create(None)?;
+
+        file.set_compression(Compression::FAST);
+
+        let expected = random_bytes(32);
+
+        expect!(file.write_bytes(&expected)).to(be_ok());
+
+        let mut reader = file.reader()?;
+        let mut actual = Vec::with_capacity(expected.len());
+
+        reader.read_to_end(&mut actual)?;
+
+        expect!(&actual).to(eq_diff(&expected));
+
+        drop(reader);
+
+        expect!(file.metadata())
+            .to(be_ok())
+            .map(|metadata| metadata.size)
+            .try_into::<usize>()
+            .to(equal(expected.len()));
+
+        Ok(())
+    })
+}
+
+#[test]
+#[cfg(feature = "deflate")]
+fn write_compressible_bytes_with_compression() -> sqlarfs::Result<()> {
+    connection()?.exec(|archive| {
+        let mut file = archive.open(Path::new("file"));
+        file.create(None)?;
+
+        file.set_compression(Compression::FAST);
+
+        let expected = vec![0u8; 32];
+
+        expect!(file.write_bytes(&expected)).to(be_ok());
+
+        let mut reader = file.reader()?;
+        let mut actual = Vec::with_capacity(expected.len());
+
+        reader.read_to_end(&mut actual)?;
+
+        expect!(&actual).to(eq_diff(&expected));
+
+        drop(reader);
+
+        expect!(file.metadata())
+            .to(be_ok())
+            .map(|metadata| metadata.size)
+            .try_into::<usize>()
+            .to(equal(expected.len()));
+
+        Ok(())
+    })
+}
+
+#[test]
+fn write_bytes_when_file_does_not_exist() -> sqlarfs::Result<()> {
+    connection()?.exec(|archive| {
+        let mut file = archive.open(Path::new("file"));
+
+        let expected = random_bytes(32);
+
+        expect!(file.write_bytes(&expected))
+            .to(be_err())
+            .map(|err| err.into_kind())
+            .to(equal(ErrorKind::NotFound));
+
+        Ok(())
+    })
+}
+
+#[test]
+fn open_reader_when_file_does_not_exist() -> sqlarfs::Result<()> {
+    connection()?.exec(|archive| {
+        let mut file = archive.open(Path::new("file"));
+
+        expect!(file.reader())
+            .to(be_err())
+            .map(|err| err.into_kind())
+            .to(equal(ErrorKind::NotFound));
+
+        Ok(())
+    })
+}
+
+#[test]
+fn write_string() -> sqlarfs::Result<()> {
+    connection()?.exec(|archive| {
+        let mut file = archive.open(Path::new("file"));
+        file.create(None)?;
+
+        let expected = "hello world";
+
+        expect!(file.write_str(expected)).to(be_ok());
+
+        let mut reader = file.reader()?;
+        let mut actual = String::with_capacity(expected.len());
+
+        reader.read_to_string(&mut actual)?;
+
+        expect!(actual.as_str()).to(eq_diff(expected));
 
         Ok(())
     })

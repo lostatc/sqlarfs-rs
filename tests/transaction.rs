10a4ee0aa567a1c1b570dadb0d304366b0ffa250
Wren Powell <wrenp@duck.com>
2024-03-31 15:43:48 -0400

    Convert unit tests to integration tests

---
+++ tests/transaction.rs
@@ -0,0 +1,117 @@
+use sqlarfs::{Connection, TransactionBehavior};
+use xpct::{be_false, be_ok, be_true, expect};
+
+fn test_transaction_commits_successfully(
+    conn: &mut Connection,
+    behavior: TransactionBehavior,
+) -> sqlarfs::Result<()> {
+    let mut tx = conn.transaction_with(behavior)?;
+
+    tx.archive_mut().open("file").create()?;
+
+    tx.commit()?;
+
+    conn.exec(|archive| {
+        expect!(archive.open("file").exists())
+            .to(be_ok())
+            .to(be_true());
+
+        Ok(())
+    })
+}
+
+#[test]
+fn transaction_with_deferred_and_commit() -> sqlarfs::Result<()> {
+    let mut conn = Connection::open_in_memory()?;
+
+    test_transaction_commits_successfully(&mut conn, TransactionBehavior::Deferred)
+}
+
+#[test]
+fn transaction_with_immediate_and_commit() -> sqlarfs::Result<()> {
+    let mut conn = Connection::open_in_memory()?;
+
+    test_transaction_commits_successfully(&mut conn, TransactionBehavior::Immediate)
+}
+
+#[test]
+fn transaction_with_exclusive_and_commit() -> sqlarfs::Result<()> {
+    let mut conn = Connection::open_in_memory()?;
+
+    test_transaction_commits_successfully(&mut conn, TransactionBehavior::Exclusive)
+}
+
+fn test_transaction_rolls_back_successfully(
+    conn: &mut Connection,
+    behavior: TransactionBehavior,
+) -> sqlarfs::Result<()> {
+    let mut tx = conn.transaction_with(behavior)?;
+
+    tx.archive_mut().open("file").create()?;
+
+    tx.rollback()?;
+
+    conn.exec(|archive| {
+        let file = archive.open("file");
+        expect!(file.exists()).to(be_ok()).to(be_false());
+
+        Ok(())
+    })
+}
+
+#[test]
+fn transaction_with_deferred_and_rollback() -> sqlarfs::Result<()> {
+    let mut conn = Connection::open_in_memory()?;
+
+    test_transaction_rolls_back_successfully(&mut conn, TransactionBehavior::Deferred)
+}
+
+#[test]
+fn transaction_with_immediate_and_rollback() -> sqlarfs::Result<()> {
+    let mut conn = Connection::open_in_memory()?;
+
+    test_transaction_rolls_back_successfully(&mut conn, TransactionBehavior::Immediate)
+}
+
+#[test]
+fn transaction_with_exclusive_and_rollback() -> sqlarfs::Result<()> {
+    let mut conn = Connection::open_in_memory()?;
+
+    test_transaction_rolls_back_successfully(&mut conn, TransactionBehavior::Exclusive)
+}
+
+fn test_exec_commits_successfully(
+    conn: &mut Connection,
+    behavior: TransactionBehavior,
+) -> sqlarfs::Result<()> {
+    conn.exec_with(behavior, |archive| archive.open("file").create())?;
+
+    conn.exec(|archive| {
+        expect!(archive.open("file").exists())
+            .to(be_ok())
+            .to(be_true());
+
+        Ok(())
+    })
+}
+
+#[test]
+fn exec_with_deferred_and_commit() -> sqlarfs::Result<()> {
+    let mut conn = Connection::open_in_memory()?;
+
+    test_exec_commits_successfully(&mut conn, TransactionBehavior::Deferred)
+}
+
+#[test]
+fn exec_with_immediate_and_commit() -> sqlarfs::Result<()> {
+    let mut conn = Connection::open_in_memory()?;
+
+    test_exec_commits_successfully(&mut conn, TransactionBehavior::Immediate)
+}
+
+#[test]
+fn exec_with_exclusive_and_commit() -> sqlarfs::Result<()> {
+    let mut conn = Connection::open_in_memory()?;
+
+    test_exec_commits_successfully(&mut conn, TransactionBehavior::Exclusive)
+}

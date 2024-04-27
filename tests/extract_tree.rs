4a7e87b7822e12eadd61d9b19186c597643b61d9
Wren Powell <wrenp@duck.com>
2024-04-27 07:08:40 -0400

    Add additional extract file tests

--- tests/extract_tree.rs
+++ tests/extract_tree.rs
@@ -1,5 +1,7 @@
-use common::connection;
-use sqlarfs::ErrorKind;
+use std::time::{Duration, SystemTime};
+
+use common::{connection, truncate_mtime};
+use sqlarfs::{ErrorKind, FileMode};
 use xpct::{be_err, be_ok, be_true, equal, expect};
 
 mod common;
@@ -102,16 +104,15 @@ fn archiving_when_source_path_is_not_valid_unicode_errors() -> sqlarfs::Result<(
 #[test]
 fn extract_regular_file() -> sqlarfs::Result<()> {
     let temp_dir = tempfile::tempdir()?;
+    let dest_path = temp_dir.path().join("dest");
 
     connection()?.exec(|archive| {
         archive.open("file")?.create_file()?;
 
-        expect!(archive.extract("file", temp_dir.path().join("dest"))).to(be_ok());
+        expect!(archive.extract("file", &dest_path)).to(be_ok());
 
-        let file = archive.open("file")?;
-
-        expect!(file.exists()).to(be_ok()).to(be_true());
-        expect!(file.metadata())
+        expect!(dest_path.exists()).to(be_true());
+        expect!(dest_path.metadata())
             .to(be_ok())
             .map(|metadata| metadata.is_file())
             .to(be_true());
@@ -123,16 +124,15 @@ fn extract_regular_file() -> sqlarfs::Result<()> {
 #[test]
 fn extract_empty_directory() -> sqlarfs::Result<()> {
     let temp_dir = tempfile::tempdir()?;
+    let dest_path = temp_dir.path().join("dest");
 
     connection()?.exec(|archive| {
         archive.open("dir")?.create_dir()?;
 
-        expect!(archive.extract("dir", temp_dir.path().join("dest"))).to(be_ok());
+        expect!(archive.extract("dir", &dest_path)).to(be_ok());
 
-        let dir = archive.open("dir")?;
-
-        expect!(dir.exists()).to(be_ok()).to(be_true());
-        expect!(dir.metadata())
+        expect!(dest_path.exists()).to(be_true());
+        expect!(dest_path.metadata())
             .to(be_ok())
             .map(|metadata| metadata.is_dir())
             .to(be_true());
@@ -144,37 +144,79 @@ fn extract_empty_directory() -> sqlarfs::Result<()> {
 #[test]
 fn extract_directory_with_children() -> sqlarfs::Result<()> {
     let temp_dir = tempfile::tempdir()?;
+    let dest_dir = temp_dir.path().join("dest");
 
     connection()?.exec(|archive| {
         archive.open("dir")?.create_dir()?;
         archive.open("dir/child-file")?.create_file()?;
         archive.open("dir/child-dir")?.create_dir()?;
 
-        expect!(archive.extract("dir", temp_dir.path().join("dest"))).to(be_ok());
+        expect!(archive.extract("dir", &dest_dir)).to(be_ok());
 
-        let dir = archive.open("dir")?;
-
-        expect!(dir.exists()).to(be_ok()).to(be_true());
-        expect!(dir.metadata())
+        expect!(dest_dir.exists()).to(be_true());
+        expect!(dest_dir.metadata())
             .to(be_ok())
             .map(|metadata| metadata.is_dir())
             .to(be_true());
 
-        let child_file = archive.open("dir/child-file")?;
-
-        expect!(child_file.exists()).to(be_ok()).to(be_true());
-        expect!(child_file.metadata())
+        expect!(dest_dir.join("child-file").exists()).to(be_true());
+        expect!(dest_dir.join("child-file").metadata())
             .to(be_ok())
             .map(|metadata| metadata.is_file())
             .to(be_true());
 
-        let child_dir = archive.open("dir/child-dir")?;
-
-        expect!(child_dir.exists()).to(be_ok()).to(be_true());
-        expect!(child_dir.metadata())
+        expect!(dest_dir.join("child-dir").exists()).to(be_true());
+        expect!(dest_dir.join("child-dir").metadata())
             .to(be_ok())
             .map(|metadata| metadata.is_dir())
             .to(be_true());
+
+        Ok(())
+    })
+}
+
+#[test]
+#[cfg(unix)]
+fn extracting_preserves_unix_file_mode() -> sqlarfs::Result<()> {
+    use std::os::unix::fs::PermissionsExt;
+
+    let temp_dir = tempfile::tempdir()?;
+    let dest_path = temp_dir.path().join("dest");
+    let expected_mode = FileMode::OWNER_R | FileMode::GROUP_R | FileMode::OTHER_R;
+
+    connection()?.exec(|archive| {
+        let mut file = archive.open("file")?;
+        file.create_file()?;
+        file.set_mode(Some(expected_mode))?;
+
+        expect!(archive.extract("file", &dest_path)).to(be_ok());
+
+        let actual_mode = dest_path.metadata()?.permissions().mode();
+        let just_permissions_bits = actual_mode & 0o777;
+
+        expect!(just_permissions_bits).to(equal(expected_mode.bits()));
+
+        Ok(())
+    })
+}
+
+#[test]
+fn extracting_preserves_file_mtime() -> sqlarfs::Result<()> {
+    let temp_dir = tempfile::tempdir()?;
+    let dest_path = temp_dir.path().join("dest");
+
+    // Some time in the past that a newly-created file could not have by default.
+    let expected_mtime = SystemTime::now() - Duration::from_secs(60);
+
+    connection()?.exec(|archive| {
+        let mut file = archive.open("file")?;
+        file.create_file()?;
+        file.set_mtime(Some(expected_mtime))?;
+
+        expect!(archive.extract("file", &dest_path)).to(be_ok());
+
+        let actual_mtime = dest_path.metadata()?.modified()?;
+        expect!(actual_mtime).to(equal(truncate_mtime(expected_mtime)));
 
         Ok(())
     })

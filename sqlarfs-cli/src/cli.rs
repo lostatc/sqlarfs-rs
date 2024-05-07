05ada9f04f85d981ca98f7ce009e4c3ad1257f34
Wren Powell <wrenp@duck.com>
2024-05-07 06:39:33 -0400

    Define CLI for `create` command

--- sqlarfs-cli/src/cli.rs
+++ sqlarfs-cli/src/cli.rs
@@ -10,12 +10,49 @@ pub struct Cli {
 }
 
 #[derive(Args, Debug, Clone)]
+pub struct Create {
+    /// The directory to archive.
+    pub source: PathBuf,
+
+    /// The path of the SQLite archive to create.
+    pub archive: PathBuf,
+
+    /// Follow symbolic links.
+    #[arg(long, default_value = "false", overrides_with = "_no_follow")]
+    pub follow: bool,
+
+    /// Don't follow symbolic links.
+    #[arg(long = "no-follow")]
+    pub _no_follow: bool,
+
+    /// Copy the given directory recursively.
+    #[arg(long, default_value = "true", overrides_with = "_no_recursive")]
+    pub recursive: bool,
+
+    /// Don't copy the given directory recursively.
+    #[arg(long = "no-recursive")]
+    pub _no_recursive: bool,
+
+    /// Preserve file metadata.
+    #[arg(long, default_value = "true", overrides_with = "_no_preserve")]
+    pub preserve: bool,
+
+    /// Don't preserve file metadata.
+    #[arg(long = "no-preserve")]
+    pub _no_preserve: bool,
+}
+
+#[derive(Args, Debug, Clone)]
 pub struct Archive {
     /// The file or directory in the filesystem to archive.
     pub source: PathBuf,
 
     /// The destination of the file in the archive.
     pub dest: PathBuf,
+
+    /// The path of the SQLite archive.
+    #[arg(long)]
+    pub db: PathBuf,
 
     /// Follow symbolic links.
     #[arg(long, default_value = "false", overrides_with = "_no_follow")]
@@ -44,7 +81,11 @@ pub struct Archive {
 
 #[derive(Subcommand, Debug, Clone)]
 pub enum Commands {
-    /// Copy a file or directory into the archive.
+    /// Create a new SQLite archive from the given directory.
+    #[command(visible_alias = "c")]
+    Create(Create),
+
+    /// Copy a file or directory into an existing archive.
     #[command(visible_alias = "ar")]
     Archive(Archive),
 }

2f0bce0ef51762c1d94d76058fc5e0ce3b677c95
Wren Powell <wrenp@duck.com>
2024-05-12 07:08:09 -0400

    Improve error message formatting in CLI

--- sqlarfs-cli/src/bin/sqlar.rs
+++ sqlarfs-cli/src/bin/sqlar.rs
@@ -8,7 +8,14 @@ use sqlarfs_cli::Cli;
 fn main() -> eyre::Result<ExitCode> {
     color_eyre::install()?;
 
-    Cli::parse().dispatch()?;
+    if let Err(err) = Cli::parse().dispatch() {
+        if let Some(user_err) = err.downcast_ref::<sqlarfs::Error>() {
+            eprintln!("{}", user_err);
+            return Ok(ExitCode::FAILURE);
+        }
+
+        return Err(err);
+    }
 
     Ok(ExitCode::SUCCESS)
 }

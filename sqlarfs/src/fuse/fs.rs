7ef7ea6fea6c101e8c6dba3ef2b5de9055655c3e
Wren Powell <wrenp@duck.com>
2024-05-15 22:17:52 -0400

    Implement getting file metadata from the archive

--- sqlarfs/src/fuse/fs.rs
+++ sqlarfs/src/fuse/fs.rs
@@ -1,12 +1,46 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::time::{Duration, SystemTime};
 
-use fuser::{ReplyDirectory, ReplyEmpty, ReplyOpen, Request};
+use fuser::{FileAttr, ReplyAttr, ReplyDirectory, ReplyEmpty, ReplyOpen, Request};
 use nix::libc;
 
 use super::error::{try_option, try_result};
 use super::handle::{DirectoryEntry, DirectoryHandle, HandleState, HandleTable};
-use super::inode::InodeTable;
-use crate::{Archive, ListOptions};
+use super::inode::{Ino, InodeTable};
+use crate::{Archive, FileMetadata, FileType, ListOptions};
+
+// Generations are a concept in libfuse in which an additional integer ID is associated with each
+// inode to ensure they're unique even when the inode numbers are reused.
+//
+// However, because this is a read-only filesystem, we never reuse inode numbers. Even if a file is
+// removed from the archive by another process, we still keep the inode number allocated.
+const DEFAULT_GENERATION: u64 = 0;
+
+// The block size used to calculate `st_blocks`.
+const BLOCK_SIZE: u32 = 512;
+
+// The value of `st_rdev` value to use if the file is not a character or block device (which will
+// always be the case for SQLite archives).
+const NON_SPECIAL_RDEV: u32 = 0;
+
+// TODO: What TTL should we use? Can the contents of the archive be modified out from under FUSE?
+const DEFAULT_TTL: Duration = Duration::ZERO;
+
+impl FileMetadata {
+    fn mode_or_default(&self) -> u16 {
+        self.mode()
+            .map(|mode| {
+                mode.bits()
+                    .try_into()
+                    .expect("Expected file mode to fit into a u16. This is a bug.")
+            })
+            .unwrap_or_else(|| match self.kind() {
+                FileType::File => 0o644,
+                FileType::Dir => 0o755,
+                FileType::Symlink => 0o777,
+            })
+    }
+}
 
 #[derive(Debug)]
 pub struct FuseAdapter<'conn, 'ar> {
@@ -16,12 +50,61 @@ pub struct FuseAdapter<'conn, 'ar> {
 }
 
 impl<'conn, 'ar> FuseAdapter<'conn, 'ar> {
+    fn get_attrs(&mut self, req: &Request, inode: Ino) -> crate::Result<FileAttr> {
+        let file_path = self.inodes.path(inode).ok_or(crate::Error::FileNotFound {
+            // We don't have a path to include in this error message, so we just use the inode.
+            // Users should never be seeing this error message regardless.
+            path: PathBuf::from(Into::<u64>::into(inode).to_string()),
+        })?;
+
+        let metadata = self.archive.open(file_path)?.metadata()?;
+
+        let size = match &metadata {
+            FileMetadata::File { size, .. } => *size,
+            FileMetadata::Dir { .. } => 0,
+            // The `st_size` of a symlink should be the length of the pathname it contains.
+            FileMetadata::Symlink { target, .. } => target.as_os_str().len() as u64,
+        };
+
+        let now = SystemTime::now();
+
+        Ok(FileAttr {
+            ino: inode.into(),
+            size,
+            blocks: size / u64::from(BLOCK_SIZE),
+            atime: now,
+            mtime: metadata.mtime().unwrap_or(now),
+            ctime: now,
+            crtime: now,
+            kind: match &metadata.kind() {
+                crate::FileType::File => fuser::FileType::RegularFile,
+                crate::FileType::Dir => fuser::FileType::Directory,
+                crate::FileType::Symlink => fuser::FileType::Symlink,
+            },
+            perm: metadata.mode_or_default(),
+            // SQLite archives don't support hard links.
+            nlink: 0,
+            uid: req.uid(),
+            gid: req.gid(),
+            rdev: NON_SPECIAL_RDEV,
+            blksize: BLOCK_SIZE,
+            flags: 0,
+        })
+    }
+}
+
+impl<'conn, 'ar> FuseAdapter<'conn, 'ar> {
     pub fn new(archive: &'ar mut Archive<'conn>, root: &Path) -> crate::Result<Self> {
         Ok(Self {
             archive,
             inodes: InodeTable::new(root),
             handles: HandleTable::new(),
         })
+    }
+
+    fn getattr(&mut self, req: &Request, ino: u64, reply: ReplyAttr) {
+        let attr = try_result!(self.get_attrs(req, ino.into()), reply);
+        reply.attr(&DEFAULT_TTL, &attr);
     }
 
     fn opendir(&mut self, _req: &Request, ino: u64, _flags: i32, reply: ReplyOpen) {
